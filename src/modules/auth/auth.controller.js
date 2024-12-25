b00d4bd2121d3797dd68157a2c25e7e0a533a128
HeyZammi <ayaanrashid960@gmail.com>
2024-12-25 10:38:14 +0530

    forgot and reset password done along with testing

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -22,3 +22,37 @@ exports.login = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.forgotPassword = async (req, res, next) =>{
+  try {
+    const {email} = req.body;
+    const user = await UserService.forgotPassword(email);
+    //we have reset password token in user
+    //TODO: send email
+    return res.status(StatusCodes.ACCEPTED)
+     .json(new ApiResponse(StatusCodes.ACCEPTED, {}, `Reset Password Mail send to ${email?.toLowerCase()}`));
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message, error))
+  }
+}
+
+exports.resetPassword = async (req, res, next) => {
+  try {
+    const {email, password, resetPasswordToken} = req.body;
+    if(!email ||!password ||!resetPasswordToken){
+      return res.status(400).json(new ApiError(StatusCodes.BAD_REQUEST, "Please fill required fields"));
+    }
+    const user = await UserService.resetPassword(email?.trim().toLowerCase(), password, resetPasswordToken);
+    if(!user){
+      return res.status(StatusCodes.UNAUTHORIZED).json(new ApiError(StatusCodes.UNAUTHORIZED, "Invalid token or email"));
+    }
+    const token = await AuthService.generateToken(user);
+    return res.status(StatusCodes.ACCEPTED)
+      .json(new ApiResponse(StatusCodes.ACCEPTED, {token}, "Password reset successfully"))
+
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message, error))    
+  }
+}
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -4,4 +4,9 @@ const AuthController = require('./auth.controller');
 const router = express.Router();
 
 router.post('/login', AuthController.login); // Login User
+
+router.post('/forgot-password', AuthController.forgotPassword); // Forgot Password
+
+router.post('/reset-password', AuthController.resetPassword); // Reset Password
+
 module.exports = router;
