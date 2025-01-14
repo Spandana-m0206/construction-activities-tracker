5a08eb395b3d8bb770f84dc0d7b622203e4a955e
Kanhaiya <singhkanhaiya91591@gmail.com>
2025-01-14 23:44:27 +0530

    did small changes required for frontend

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -8,6 +8,9 @@ class UserService extends BaseService {
         super(UserModel); // Pass the User model to the BaseService
     }
 
+    async findUserById(id) {
+        return await this.model.findById(id).populate('profilePhoto', '_id url'); // Populate organization details
+    }
     // Example of a custom service method: Find users by role
     async findUsersByRole(role) {
         return await this.model.model.find({ role }).populate('org', 'name'); // Populate organization details
